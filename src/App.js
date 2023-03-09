bf9f2e4144b3fc10a17dc8b01082ef47e7863381
Sergio_MoraAgudo <alumno.107030@ies-azarquiel.es>
2023-03-09 12:51:13 +0100

    creacion menu

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
-import header from './img/header.png';
 import './App.css';
+import Menu from "./Components/Menu";
 
 function App() {
   return (
     <div className="App">
-      <div className="text-center mt-3">
-        <img src={header} alt="Header" style={{ width: "400px", height: "auto" }}/>
-      </div>
+      <Menu/>
     </div>
   );
 }
