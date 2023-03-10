634dee26dd42e5b41ab8876d3f09d79d3b913df5
Sergio_MoraAgudo <alumno.107030@ies-azarquiel.es>
2023-03-10 21:17:25 +0100

    retoques en los cards y fondo

--- src/componentes/Card/Card.jsx
+++ src/componentes/Card/Card.jsx
@@ -1,20 +1,39 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react';
+import './Card.css'
 const Card = ({results}) => {
   let mostrar;
+  let [classStatus,setClassStatus] = useState("desconocido");
   console.log(results);
   if(results){
     mostrar = results.map((personaje)=>{
        let { id, name, image, location, status} = personaje;
+       let classStatus;
+       switch(status){
+         case "Dead":
+           classStatus="muerto";
+           break;
+         case "Alive":
+           classStatus="vivo";
+           break;
+         default:
+           classStatus="desconocido";
+           break;
+       }
+       /*
+       useEffect(() => {
+          setClassStatus(status)
+      }, [personaje])
+      */
        return(
         <div key={id} className="col-3">
-          <div className="card p-3 border border-dark mb-4">
-            <img src={image} alt="" className='img-fluid rounded-start rounded border border-dark'/>
+          <div className="card p-3 border border-light mb-4">
+              <p className={classStatus+" estado centrar"}>{status}</p>
+            <img src={image} alt="" className='img-fluid rounded-start rounded border border-light'/>
             <div class="card-body">
               <h3 class="card-title">{name}</h3>
               <h6 class="card-text">Ultima vez visto:</h6>
               <p className="card-text">{location.name}</p>
-              <p>{status}</p>
+              
             </div>
             
           </div>
